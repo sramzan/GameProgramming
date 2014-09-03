3eb870137a5d7fe6bd582f65d5138e21755c34c3
Ivan Safrin <ivansafrin@gmail.com>
2014-09-03 02:27:20 -0400

    Updated Xcode project

--- project_template/Xcode/NYUCodebase/main.cpp
+++ project_template/Xcode/NYUCodebase/main.cpp
@@ -1,28 +1,30 @@
 
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_opengl.h>
+#include <SDL.h>
+#include <SDL_opengl.h>
 #include <SDL_image.h>
 
 SDL_Window* displayWindow;
-SDL_Renderer* displayRenderer;
-
 
 int main(int argc, char *argv[])
 {
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindowAndRenderer(800, 600, SDL_WINDOW_OPENGL, &displayWindow, &displayRenderer);
-
-    bool done = false;
-    SDL_Event event;
+	SDL_Init(SDL_INIT_VIDEO);
+	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+	SDL_GL_MakeCurrent(displayWindow, context);
     
-    while(!done) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-                done = true;
-            }
-        }
-    }
+	bool done = false;
+	
+	SDL_Event event;
     
-    SDL_Quit();
-    return 0;
+	while (!done) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+				done = true;
+			}
+		}
+		SDL_GL_SwapWindow(displayWindow);
+	}
+    
+	SDL_Quit();
+	return 0;
 }
