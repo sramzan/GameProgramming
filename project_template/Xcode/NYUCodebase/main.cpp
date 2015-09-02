497188e379acd03a0fefbf55c6a9df28732dda55
Ivan Safrin <ivansafrin@gmail.com>
2015-09-02 02:23:51 -0400

    Added ES2 reference card and helper classes to Xcode template

--- project_template/Xcode/NYUCodebase/main.cpp
+++ project_template/Xcode/NYUCodebase/main.cpp
@@ -7,24 +7,25 @@ SDL_Window* displayWindow;
 
 int main(int argc, char *argv[])
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
-	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
-	SDL_GL_MakeCurrent(displayWindow, context);
+    SDL_Init(SDL_INIT_VIDEO);
+    displayWindow = SDL_CreateWindow("My Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 360, SDL_WINDOW_OPENGL);
+    SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
+    SDL_GL_MakeCurrent(displayWindow, context);
+    SDL_Event event;
     
-	bool done = false;
-	
-	SDL_Event event;
+    bool done;
+    while (!done) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+                done = true;
+            }
+        }
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(displayWindow);
+    }
     
-	while (!done) {
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-				done = true;
-			}
-		}
-		SDL_GL_SwapWindow(displayWindow);
-	}
-    
-	SDL_Quit();
-	return 0;
+    SDL_Quit();
+    return 0;
 }
+
+
