8a6ddf7d65aa7f58165bdc9d5f50aef12aa9eabb
Sean Ramzan <sramzan@nyu.edu>
2016-02-06 17:19:39 -0500

    Adding necessary frameworks, and some minor changes

--- project_template/Xcode/NYUCodebase/main.cpp
+++ project_template/Xcode/NYUCodebase/main.cpp
@@ -4,12 +4,15 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#include "Matrix.h"
+#include "ShaderProgram.h"
 
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
 #else
 #define RESOURCE_FOLDER "NYUCodebase.app/Contents/Resources/"
 #endif
+
 
 SDL_Window* displayWindow;
 
@@ -34,6 +37,32 @@ int main(int argc, char *argv[])
         glClear(GL_COLOR_BUFFER_BIT);
         SDL_GL_SwapWindow(displayWindow);
     }
+//    
+//    Matrix modelMatrix;
+//    modelMatrix.identity();
+    
+    
+//    float vertices[] = {0.5, -0.5, 0.0};
+//    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
+//
+    
+    ShaderProgram program(RESOURCE_FOLDER"vertex.glsl", RESOURCE_FOLDER"fragment.glsl");
+    
+    Matrix projectionMatrix;
+    projectionMatrix.setOrthoProjection(-3.55, 3.55, -2.0f, 2.0f, -1.0f, 1.0f);
+    
+    Matrix modelMatrix;
+    Matrix viewMatrix;
+    
+    // Happens every frame -- passing matrices to our program
+    program.setModelMatrix(modelMatrix);
+    program.setProjectionMatrix(projectionMatrix);
+    program.setViewMatrix(viewMatrix);
+    
+    glUseProgram(program.programID);
+    
+    float vertices[] = {0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f};
+    glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
     
     SDL_Quit();
     return 0;
